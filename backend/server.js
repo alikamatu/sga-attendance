6bbdc0d6bba19a90e018d28b00f2bfbd1ece0cb1
alikamatu <alikamatu14@gmail.com>
2025-03-26 09:12:13 +0000

    update

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-{
-    "build": {
-      "env": {
-        "NODE_ENV": "production"
-      }
-    }
-  }
-  
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { pool } = require("pg");
+
+const app = express();
+app.use(cors());  
+app.use(express.json());
