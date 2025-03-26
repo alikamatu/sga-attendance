53a6463113a5d8bc937a793017dd32a3f0a63ea4
alikamatu <alikamatu14@gmail.com>
2025-03-26 11:20:39 +0000

    auth backend

--- backend/routes/attendanceRoutes.js
+++ backend/routes/attendanceRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { addAttendance, getAttendance, deleteAttendance } = require("../controllers/attendanceController");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-router.post("/", addAttendance);
-router.get("/", getAttendance);
-router.delete("/:id", deleteAttendance);
+// Attendance Routes (Protected)
+router.post("/", authMiddleware, addAttendance);
+router.get("/", authMiddleware, getAttendance);
+router.delete("/:id", authMiddleware, deleteAttendance);
 
 module.exports = router;
