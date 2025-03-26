1c017f0facf4001f2378ae22de872a1577b7c0b7
alikamatu <alikamatu14@gmail.com>
2025-03-26 10:40:16 +0000

    update

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,28 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const attendanceRoutes = require("./routes/attendanceRoutes");
+const authRoutes = require("./routes/authRoutes");
+const pool = require("./config/db");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/attendance", attendanceRoutes);
+pool.connect()
+  .then(client => {
+    console.log('✅ Connected to PostgreSQL');
+    return client.query('SELECT NOW()')
+      .then(res => {
+        console.log('⏱️ Current time:', res.rows[0].now);
+        client.release();
+      });
+  })
+  .catch(err => {
+    console.error('❌ Connection error:', err.message);
+  });
+
+  app.use("/api/auth", authRoutes);
+  app.use("/api/attendance", attendanceRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,21 +1,8 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import axios from "axios";
-
 export default function Home() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    axios.get("http://localhost:5000")
-      .then((response) => setMessage(response.data.message))
-      .catch((error) => console.error("Error:", error));
-  }, []);
 
   return (
     <div>
       <h1 className="text-8xl font-thin">Attendance System</h1>
-      <p>{message}</p>
     </div>
   );
 }
