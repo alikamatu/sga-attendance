4b82af7dbbc2165765b2afd00b6ede01f7dda181
alikamatu <alikamatu14@gmail.com>
2025-03-26 10:22:54 +0000

    update

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 
   return (
     <div>
-      <h1>Attendance System</h1>
+      <h1 className="text-8xl font-thin">Attendance System</h1>
       <p>{message}</p>
     </div>
   );
