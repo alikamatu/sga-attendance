a5dbc9a56e7b643ffbe05846ca697f48f2a615e6
alikamatu <alikamatu14@gmail.com>
2025-03-26 08:47:18 +0000

    frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+
+  return (
+    <div className="flex w-screen h-screen items-center justify-center">
+      <h1 className="text-9xl text-center">Hello World.</h1>
+    </div>
+  )
+}
