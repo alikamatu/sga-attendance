c76e5f389e5bc8a8820513b6cdf0162a321b6652
alikamatu <alikamatu14@gmail.com>
2025-03-26 10:12:38 +0000

    update

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,21 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export default function Home() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:5000")
+      .then((response) => setMessage(response.data.message))
+      .catch((error) => console.error("Error:", error));
+  }, []);
 
   return (
-    <div className="flex w-screen h-screen items-center justify-center">
-      <h1 className="text-9xl text-center">Hello World!</h1>
+    <div>
+      <h1>Attendance System</h1>
+      <p>{message}</p>
     </div>
-  )
+  );
 }
