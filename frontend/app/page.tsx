2032436081f4f147e3a64fe035a57f58e6e59613
alikamatu <alikamatu14@gmail.com>
2025-03-26 09:03:33 +0000

    testing

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
 
   return (
     <div className="flex w-screen h-screen items-center justify-center">
-      <h1 className="text-9xl text-center">Hello World.</h1>
+      <h1 className="text-9xl text-center">Hello World!</h1>
     </div>
   )
 }
